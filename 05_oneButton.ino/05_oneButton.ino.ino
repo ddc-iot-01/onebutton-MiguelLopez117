31a2a27df76a6d23d5d31ea591ed994942ecf7bf
Miguel <mlopez38@unm.edu>
2020-03-03 15:25:17 -0700

    added oneButtoneLED

--- 05_oneButton.ino/05_oneButton.ino.ino
+++ 05_oneButton.ino/05_oneButton.ino.ino
@@ -1,46 +1,87 @@
 /*
  * Project: Using OneButton library
  * Description: Introduct Students to OneButton Library using a button and the serial monitor
- * Author: 
- * Date: 
+ * Author: Miguel Lopez
+ * Date: 3-Mar-2020
  */
 
-#include "OneButton.h"
+#include <OneButton.h>
+ 
+ // Setup OneButton on pin 23
+ OneButton migsButton(23,false);
 
-// Setup OneButton on pin 23
-
-// Create variables buttonState and flash
-
+int buttonState = LOW;
+int ledPin6 = 6;
+int ledPin5 = 5;
+int ledPin4 = 4;
+int flash = LOW;
 
 void setup() {
 
-// Setup Serial Monitor
-// Link oneButton library to functions click1, doubleclick1, longPressStart1
-oneButton button1(23,false);
+Serial.begin(9600);
+while (!Serial);
+
+migsButton.attachClick(click1);
+migsButton.attachDoubleClick(doubleclick1);
+//migsButton.attachLongPressStart(longPressStart1);
+//migsButton.attachLongPressStop(longPressStop1);
+migsButton.setClickTicks(250);
+migsButton.setPressTicks(2000);
+
+if(Serial)Serial.println("Starting OneButton...");
+pinMode(6, OUTPUT);
 } 
 
 void loop() {
-  // keep watching the push buttons:
-  button1.tick();
-} // loop
+  migsButton.tick();
+  if (buttonState == LOW){
+    digitalWrite(ledPin6,LOW);
+  }
+  else
+  {
+    digitalWrite(ledPin6, HIGH);
+  }
+  if (flash == LOW){
+    digitalWrite(ledPin6, LOW);
+  } 
+  else
+  {
+    digitalWrite(ledPin6, HIGH);
+    delay(50);
+    digitalWrite(ledPin6, LOW);
+    delay(50);
+   
+  }
 
-
+ 
+}// loop
 // ----- button 1 callback functions
 
 // This function will be called when the button1 was pressed 1 time.
 void click1() {
-//change and print buttonState
-  
+  Serial.println("migsButton click.");
+  buttonState = (!buttonState);
+  Serial.print("buttonState = ");
+  Serial.println(buttonState);
+
 } // click1
 
 
 // This function will be called when the button1 was pressed 2 times in a short timeframe.
 void doubleclick1() {
-// change state of flash and print
+  Serial.println("migsButton doubleclick.");
+  flash = !flash;
+  Serial.print("flash = ");
+  Serial.println(flash);
+
 } // doubleclick1
 
 
 // This function will be called once, when the button1 is pressed for a long time.
 void longPressStart1() {
-// print longPress
+Serial.println("Button 1 longPress...");
 } // longPressStart1
+
+void longPressStop(){
+  Serial.println("Button 1 longPress stop");
+}
--- 05_oneButtonLED.ino/05_oneButtonLED.ino.ino
+++ 05_oneButtonLED.ino/05_oneButtonLED.ino.ino
@@ -1,44 +1,87 @@
 /*
  * Project: Using OneButton library
- * Description: Use oneButton to light and blink an LED
- * Author: 
- * Date: 
+ * Description: Introduct Students to OneButton Library using a button and the serial monitor
+ * Author: Miguel Lopez
+ * Date: 3-Mar-2020
  */
 
-#include "OneButton.h"
+#include <OneButton.h>
+ 
+ // Setup OneButton on pin 23
+ OneButton migsButton(23,false);
 
-// Setup OneButton on pin 23 
-
-
+int buttonState = LOW;
+int ledPin6 = 6;
+int ledPin5 = 5;
+int ledPin4 = 4;
+int flash = LOW;
 
 void setup() {
 
-  Serial.begin(9600);
+Serial.begin(9600);
+while (!Serial);
 
+migsButton.attachClick(click1);
+migsButton.attachDoubleClick(doubleclick1);
+//migsButton.attachLongPressStart(longPressStart1);
+//migsButton.attachLongPressStop(longPressStop1);
+migsButton.setClickTicks(250);
+migsButton.setPressTicks(2000);
 
+if(Serial)Serial.println("Starting OneButton...");
+pinMode(6, OUTPUT);
 } 
 
 void loop() {
-  // keep watching the push button:
-  button1.tick();
-  
-  // light or blink LED based on buttonState and Flash 
-} 
+  migsButton.tick();
+  if (buttonState == LOW){
+    digitalWrite(ledPin6,LOW);
+  }
+  else
+  {
+    digitalWrite(ledPin6, HIGH);
+  }
+  if (flash == LOW){
+    digitalWrite(ledPin6, LOW);
+  } 
+  else
+  {
+    digitalWrite(ledPin6, HIGH);
+    delay(50);
+    digitalWrite(ledPin6, LOW);
+    delay(50);
+   
+  }
 
-
+ 
+}// loop
 // ----- button 1 callback functions
 
 // This function will be called when the button1 was pressed 1 time.
 void click1() {
+  Serial.println("migsButton click.");
+  buttonState = (!buttonState);
+  Serial.print("buttonState = ");
+  Serial.println(buttonState);
 
-// change button state
-  
 } // click1
 
 
 // This function will be called when the button1 was pressed 2 times in a short timeframe.
 void doubleclick1() {
-
-// change flash state
+  Serial.println("migsButton doubleclick.");
+  flash = !flash;
+  Serial.print("flash = ");
+  Serial.println(flash);
 
 } // doubleclick1
+
+
+// This function will be called once, when the button1 is pressed for a long time.
+void longPressStart1() {
+Serial.println("Button 1 longPress...");
+} // longPressStart1
+
+void longPressStop(){
+  Serial.println("Button 1 longPress stop");
+}
